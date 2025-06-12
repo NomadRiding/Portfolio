a6b0575298047ee089c366d9292c9bac614034bf
nomadriding <fundora.a@gmail.com>
2025-06-11 20:10:16 -0400

    Add HomePage Location

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function HomePage() {
+  return (
+    <div className="HomePage-container">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple home page component.</p>
+    </div>
+  )
+}
